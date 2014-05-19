533de4887823835a7590ad25e726ff96a798193e
lucken <h.luqman@gmail.com>
2014-05-19 10:59:43 +0700

    tes

---
+++ SmsGuard/src/com/example/smsguard/model/Contact.java
@@ -0,0 +1,33 @@
+package com.example.smsguard.model;
+
+public class Contact {
+	String id;
+	String name;
+	String phone;
+
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+}
---
+++ SmsGuard/src/com/example/smsguard/model/Message.java
@@ -0,0 +1,50 @@
+package com.example.smsguard.model;
+
+public class Message {
+	String id;
+	String sender;
+	String date;
+	String time;
+	String content;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getSender() {
+		return sender;
+	}
+
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+}
